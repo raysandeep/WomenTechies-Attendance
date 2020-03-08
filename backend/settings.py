0c3f1ac98d70d9b2354067f0ae4d49855524d4e6
Sandeep <rayanuthalas@gmail.com>
2020-03-08 23:17:26 +0530

    Done

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,4 +128,4 @@ STATICFILES_DIRS = [
 STATIC_ROOT= os.path.join(BASE_DIR,'assets')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
